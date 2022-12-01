eeff6985219f5357ad5a3bf4e9e332f9a9613a5f
abidullahafridi <116450166+abidullahafridi@users.noreply.github.com>
2022-12-01 17:34:39 +0500

    java8
    java8

---
+++ app.js
@@ -0,0 +1,9 @@
+var display = document.getElementById("display");
+
+function getval(a) {
+    display.value +=  a;
+}
+function calculate() {
+    var a = eval(display.value)
+    display.value = a;
+}
